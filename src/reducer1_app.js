d6a47464ffaed16215cd60ff91584cf14c850e7f
Ashley Bythell <abythell@Hotmail.com>
2024-08-23 13:57:30 +1000

    Create reducer1_app.js

---
+++ src/reducer1_app.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { useReducer } from "react";
+
+
+
+function App({library}) {
+const [checked, setChecked] = useReducer(
+  (checked) => !checked,
+  false
+);
+
+return (
+    <div className="App">
+      <input type="checkbox"
+      value={checked} 
+      onChange={setChecked}      
+      />
+      <label>
+        {checked ? "checked" : "not checked"} </label>
+     </div>
+  );
+}
+
+export default App;
